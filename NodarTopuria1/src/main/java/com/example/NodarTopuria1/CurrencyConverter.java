0bd77bb4967af74ca93ee45fb5633a121e30cac1
Topuria1996 <74104360+Topuria1996@users.noreply.github.com>
2021-07-07 20:55:45 +0400

    Update CurrencyConverter.java

--- NodarTopuria1/src/main/java/com/example/NodarTopuria1/CurrencyConverter.java
+++ NodarTopuria1/src/main/java/com/example/NodarTopuria1/CurrencyConverter.java
@@ -2,10 +2,11 @@ package com.example.NodarTopuria1;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import java.util.List;
 
 @WebService
 public interface CurrencyConverter {
 
     @WebMethod
-     Double convert(Double value,String fromCode);
+     List<Double> convert(Double value,String fromCode);
 }
